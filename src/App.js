0a44a431e50030110f9850b19ac5971fe67170f9
cvcsu <computervisionclub.csu@gmail.com>
2024-03-25 01:15:08 -0600

    update an url in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-// App.js
-
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom"; // Use HashRouter
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import Roster from "./components/Roster";
@@ -11,7 +9,7 @@ import "./navStyles.css";
 
 function App() {
   return (
-    <Router basename="/CCVC">
+    <Router basename="/"> {/* Remove /CCVC if using HashRouter */}
       <nav className="navigation">
         <Link to="/">Home</Link> | 
         <Link to="/about-us">About Us</Link> | 
@@ -19,20 +17,17 @@ function App() {
         <div className="dropdown">
           <button className="dropbtn">Competitions</button>
           <div className="dropdown-content">
-            <Link to="/S24_competition">Spring 24</Link>
-            {/* <Link to="/competitions/Fall24">Fall 24</Link>
-            <Link to="/competitions/Spring25">Spring 25</Link> */}
-            {/* Add more semesters as needed */}
+            <Link to="/competitions/spring24">Spring 24</Link> {/* Ensure consistency in path naming */}
+            {/* Uncomment or add other links as necessary */}
           </div>
         </div>
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Route for the Home page */}
+        <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/roster" element={<Roster />} />
-        <Route path="/S24_competition" element={<Spring24 />} />
-        {/* <Route path="/competitions" element={<Competitions />} /> */}
-        {/* <Route path="/competitions/:semester" element={<CompetitionDetail />} /> */}
+        <Route path="/competitions/spring24" element={<Spring24 />} /> {/* Adjusted path */}
+        {/* Uncomment or adjust additional routes as necessary */}
       </Routes>
     </Router>
   );
